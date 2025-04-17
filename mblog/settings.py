6a2adfa5deafdaf63247471783efb2844228337b
Yu-cre7 <ren_diqiuren@163.com>
2025-04-17 14:58:53 +0800

    2 commit

--- mblog/settings.py
+++ mblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8!k=3_b(ziqevhj61#awo8&=o@727+!vv9@8k6w(qvhsmk%ue+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mysite',
 ]
 
 MIDDLEWARE = [
@@ -54,10 +55,11 @@ ROOT_URLCONF = 'mblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [BASE_DIR / 'templates'], # new
+        'APP_DIRS': True, 
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug', # new
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -102,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -114,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from mysite.models import Post
+from datetime import datetime
 
-# Create your views here.
+# def homepage(request):
+#     posts = Post.objects.all()
+#     post_lists = list()
+#     for count, post in enumerate(posts):
+#         post_lists.append("No.{}:".format(str(count)) + str(post)+"<hr>")
+#         post_lists.append("<small>" + str(post.body) + "</small><br><br>")
+#     return HttpResponse(post_lists)
+
+def homepage(request):
+    posts = Post.objects.all()
+    now =  datetime.now()
+    return render(request, 'index.html', locals())
+
+def showpost(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            # return render(request, 'post.html', locals())
+            return render(request, 'post.html', {'post':post})
+    except:
+        return render('/')
